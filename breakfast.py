f23d672edf7f1839c46f7e359ad87ec22fdadf86
test <test>
2023-12-18 22:42:06 +0700

    test

---
+++ breakfast.py
@@ -0,0 +1,17 @@
+menu = {
+   "кофе": 20.00,
+   "чай": 10.00,
+   "булочка": 5.00,
+   "салат": 30.40,
+   "пирожное": 45.50
+}
+total_cost = 0
+while True:
+    try:
+        dish = input("Блюдо: ").lower()
+        if dish in menu:
+            total_cost += menu[dish]
+    except EOFError:
+        break
+print("")
+print(f"Сумма: {total_cost:.2f}")
